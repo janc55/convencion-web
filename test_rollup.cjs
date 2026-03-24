b7b1a04096e3efbc23f1dff14484f9e01ad38896
janc55 <josenegretti55@gmail.com>
2026-03-24 11:48:49 -0400

    cambio de QR y datos

--- test_rollup.cjs
+++
@@ -1,8 +0,0 @@
-try {
-    const native = require('./node_modules/rollup/dist/native.js');
-    console.log('Successfully loaded rollup/dist/native.js');
-    console.log('Exports:', Object.keys(native));
-} catch (err) {
-    console.error('Failed to load rollup/dist/native.js');
-    console.error(err);
-}
